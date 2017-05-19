795d003dcab9744322ed47b90d5b9015dac66b23
bcfolsom <bcfolsom@gmail.com>
2017-05-18 19:05:54 -0500

    count to 10

---
+++ count1_10.js
@@ -0,0 +1,8 @@
+// Write a function printNumbers which is given a start number and an end number. 
+// It will print the numbers from one to the other, one per line:
+
+count = 0
+while (count < 10) {
+  count += 1;
+  console.log(count);
+}
